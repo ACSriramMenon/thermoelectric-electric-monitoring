c04af15e22964c00ee3e93e995b98597ab454b39
AC Sriram Menon <srirammenon5@gmail.com>
2026-04-21 19:38:47 +0530

    Add readability to arduino

--- Arduino/teg_monitor/teg_monitor.ino
+++ Arduino/teg_monitor/teg_monitor.ino
@@ -2,16 +2,18 @@ void setup(){
   Serial.begin(9600);
 }
 
-void loop(){
-  int rawTemp = analogRead(A0);
-  int rawVolt = analogRead(A1);
+//Loop runs infinitely
 
-  float temperature = rawTemp * (5.0 / 1023.0) * 100.0;
-  float voltage = rawVolt * (5.0 / 1023.0);
+void loop(){
+  int AnalogTemp = analogRead(A0);      //Reads temperature analog value
+  int AnalogVolt = analogRead(A1);      //Reads Voltage analog value
+
+  float temperature = AnalogTemp * (5.0 / 1023.0) * 100.0;    //Converts temperature analog value to Temperature value
+  float voltage = AnalogVolt * (5.0 / 1023.0);                //Converts voltage analog value to Voltage value
 
   Serial.print("Temperature:");
-  Serial.print(temperature, 2);
-  Serial.print(", Voltage:");
+  Serial.print(temperature, 2);       //Returns temperature with 2 decimal
+  Serial.print(", Voltage:");         //Returns voltage with 2 decimal
   Serial.println(voltage, 2);
 
   delay(1000);
